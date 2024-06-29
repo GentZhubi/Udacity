759726ab64f053af23b0a224d6a0d9324e45e186
Gent Zhubi <ghent@gmail.com>
2024-06-30 01:02:23 +0200

    Fix for accelerometer_landing_to_trusted SQL Transform to show only accelerometer columns

--- DataEngineeringNanodegree/STEDI-Project/accelerometer/accelerometer_landing_to_trusted.py
+++ DataEngineeringNanodegree/STEDI-Project/accelerometer/accelerometer_landing_to_trusted.py
@@ -25,19 +25,19 @@ CustomerLanding_node1719616736301 = glueContext.create_dynamic_frame.from_catalo
 AccelerometerLanding_node1719616767569 = glueContext.create_dynamic_frame.from_catalog(database="stedi-db", table_name="acceloremeter_landing", transformation_ctx="AccelerometerLanding_node1719616767569")
 
 # Script generated for node SQL Query
-SqlQuery2592 = '''
+SqlQuery2725 = '''
 WITH customer_trusted_tmp AS (
     SELECT *
     FROM customer_landing
     WHERE shareWithResearchAsOfDate != 0
 )
-SELECT *
-FROM accelerometer_landing
+SELECT acceloremeter_landing.*
+FROM acceloremeter_landing
 JOIN customer_trusted_tmp
-ON accelerometer_landing.user = customer_trusted_tmp.email
+ON acceloremeter_landing.user = customer_trusted_tmp.email
 
 '''
-SQLQuery_node1719616789375 = sparkSqlQuery(glueContext, query = SqlQuery2592, mapping = {"accelerometer_landing":AccelerometerLanding_node1719616767569, "customer_landing":CustomerLanding_node1719616736301}, transformation_ctx = "SQLQuery_node1719616789375")
+SQLQuery_node1719616789375 = sparkSqlQuery(glueContext, query = SqlQuery2725, mapping = {"acceloremeter_landing":AccelerometerLanding_node1719616767569, "customer_landing":CustomerLanding_node1719616736301}, transformation_ctx = "SQLQuery_node1719616789375")
 
 # Script generated for node AccelerometerTrusted
 AccelerometerTrusted_node1719616854089 = glueContext.getSink(path="s3://stedi-data/acceloremeter/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="snappy", enableUpdateCatalog=True, transformation_ctx="AccelerometerTrusted_node1719616854089")
